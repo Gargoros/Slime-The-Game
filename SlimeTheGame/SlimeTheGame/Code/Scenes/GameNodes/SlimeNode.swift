a10216e72bfccd5e7de0e01699a9962fac7e4be1
Mikhail Zhachko <mikhailzhachko@gmail.com>
2025-08-07 00:31:00 +0300

    fix: make more clear logic, fixed spawn logic, fixed sound play logic

--- SlimeTheGame/SlimeTheGame/Code/Scenes/GameNodes/SlimeNode.swift
+++ SlimeTheGame/SlimeTheGame/Code/Scenes/GameNodes/SlimeNode.swift
@@ -11,6 +11,11 @@ final class SlimeNode: SKSpriteNode {
     private var idleTexture:     [SKTexture]?
     private var walkTexture:     [SKTexture]?
     private var deathTexture:    [SKTexture]?
+    private let walkSound01      = SKAction.playSoundFileNamed(AppConstants.soundNames.walkSound01, waitForCompletion: false)
+    private let walkSound02      = SKAction.playSoundFileNamed(AppConstants.soundNames.walkSound02, waitForCompletion: false)
+    private let idleSound        = SKAction.playSoundFileNamed(AppConstants.soundNames.idleSound, waitForCompletion: false)
+    private let deathSound       = SKAction.playSoundFileNamed(AppConstants.soundNames.deathSound, waitForCompletion: false)
+    
     //MARK: - Init
     init() {
         let texture          = SKTexture(imageNamed: AppConstants.imageNames.startSlime)
@@ -39,21 +44,27 @@ final class SlimeNode: SKSpriteNode {
     }
     //MARK: - states
     func idleState(){
+        stopAllSounds()
         guard let idleTexture = idleTexture else { preconditionFailure(AppConstants.errors.animationError) }
         removeAction(forKey: SlimeAnimationType.death.rawValue)
         removeAction(forKey: SlimeAnimationType.walk.rawValue)
         startAnimation(textures: idleTexture, speed: 0.1, name: SlimeAnimationType.idle.rawValue, count: 0, resize: true, restore: true)
+        playSound(action: idleSound, key: AppConstants.dataKeys.slimeIdleSound.rawValue)
     }
     func walkState(){
+        stopAllSounds()
         guard let walkTexture = walkTexture else { preconditionFailure(AppConstants.errors.animationError) }
         removeAction(forKey: SlimeAnimationType.idle.rawValue)
         startAnimation(textures: walkTexture, speed: 0.1, name: SlimeAnimationType.walk.rawValue, count: 0, resize: true, restore: true)
+        walkSoundEffect()
     }
-    func deathState(){
+    func deathState(completion: @escaping () -> Void = {}){
+        stopAllSounds()
         guard let deathTexture = deathTexture else { preconditionFailure(AppConstants.errors.animationError) }
         removeAction(forKey: SlimeAnimationType.walk.rawValue)
         removeAction(forKey: SlimeAnimationType.idle.rawValue)
-        startAnimation(textures: deathTexture, speed: 0.1, name: SlimeAnimationType.death.rawValue, count: 1, resize: true, restore: true)
+        startAnimation(textures: deathTexture, speed: 0.1, name: SlimeAnimationType.death.rawValue, count: 1, resize: true, restore: false)
+        playSound(action: deathSound, key: AppConstants.dataKeys.slimeDeathSound.rawValue)
     }
     func moveToPosition(pos: CGPoint, direction: String, speed: TimeInterval){
         switch direction {
@@ -63,5 +74,19 @@ final class SlimeNode: SKSpriteNode {
         let newPos              = CGPoint(x: pos.x, y: position.y)
         let moveAction          = SKAction.move(to: newPos, duration: speed)
         run(moveAction)
+    }
+    private func walkSoundEffect() {
+        let random = Int.random(in: 1...2)
+        let sound = random == 1 ? walkSound01 : walkSound02
+        self.run(sound, withKey: AppConstants.dataKeys.slimeWalkSound.rawValue)
+    }
+    private func playSound(action: SKAction, key: String) {
+        self.run(action, withKey: key)
+    }
+
+    func stopAllSounds() {
+        self.removeAction(forKey: AppConstants.dataKeys.slimeWalkSound.rawValue)
+        self.removeAction(forKey: AppConstants.dataKeys.slimeIdleSound.rawValue)
+        self.removeAction(forKey: AppConstants.dataKeys.slimeDeathSound.rawValue)
     }
 }
